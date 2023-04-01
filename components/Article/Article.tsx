8b872793972cc0e81365498e8a1ceaf0f44e4e67
yuta4j1 <kascado.ys10@gmail.com>
2023-04-02 00:50:52 +0900

    fix: blockquote style

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -141,11 +141,12 @@ export const Article: React.FC<{ title: string; mdText: string }> = ({
               return (
                 <blockquote
                   className={classNames(
-                    'p-2',
+                    'py-2',
+                    'px-4',
                     'my-2',
                     'text-gray-500',
                     'border-solid',
-                    'border-l-4',
+                    'border-l-2',
                     'border-gray-400'
                   )}
                 >
