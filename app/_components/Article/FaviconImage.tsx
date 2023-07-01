ca1b4e95b491cc6ab9e2392c92e9add2577ef019
yuta4j1 <kascado.ys10@gmail.com>
2023-07-01 22:35:42 +0900

    fix

--- app/_components/Article/FaviconImage.tsx
+++ app/_components/Article/FaviconImage.tsx
@@ -3,7 +3,5 @@ import React from 'react'
 import classNames from 'classnames'
 
 export const FaviconImage: React.FC<{ src: string }> = ({ src }) => {
-  return (
-    <img className={classNames('h-4')} src={src} alt="" onError={e => {}} />
-  )
+  return <img className={classNames('h-4')} src={src} alt="favicon image" />
 }
