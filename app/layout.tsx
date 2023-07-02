50d0b25a6780578af524936e257d731cc30de727
yuta4j1 <kascado.ys10@gmail.com>
2023-07-02 11:57:08 +0900

    feat: custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ import Footer from './_components/Footer'
 export const metadata: Metadata = {
   title: 'yuta4j1.dev',
   description: 'yuta4j1 develop blog',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
 }
 
 // TODO: next-seo
@@ -18,8 +23,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <body className={classNames("font-sans")}>
+      <link
+        href="https://fonts.googleapis.com/css?family=M+PLUS+1p&display=swap"
+        rel="stylesheet"
+      ></link>
+      <body>
         <Header />
         {children}
         <Footer />
